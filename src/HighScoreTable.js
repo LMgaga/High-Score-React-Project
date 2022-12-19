f3510de0e961f81ecad3d2dd5bfd51b324ed1ce6
Luleka Mgaga <lulekamgaga@hotmail.com>
2022-12-19 13:00:16 +0000

    fixed errors

---
+++ src/HighScoreTable.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import allCountryScores from "./allCountryScore";
+import PlayerScore from "./PlayerScore";
+
+function HighScoreTable() {
+  const [order, setOrder] = useState("ascending");
+
+  function ChangeOrder() {
+    return order === "ascending"
+      ? setOrder("descending")
+      : setOrder("ascending");
+  }
+
+  return (
+    <div>
+      <div className="heading">
+        <h1>High Scores per Country</h1>
+        <div className="button">
+          <button onClick={ChangeOrder}>Reorder scores</button>
+        </div>
+      </div>
+      {allCountryScores
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((country, index) => {
+          return (
+            <div className="score-card">
+              <h2>HIGH SCORE: {country.name}</h2>
+              <PlayerScore key={index} scores={country.scores} order={order} />
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default HighScoreTable;
